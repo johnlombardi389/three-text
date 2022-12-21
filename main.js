c9ff18ac3b61f08a56fd8cf0d8e3dddbd4179711
johnlombardi389 <lombardij38@gmail.com>
2022-12-21 15:44:25 -0500

    Set up size listener

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const canvas = document.querySelector(".webgl");
 
 // Scene
 const scene = new THREE.Scene();
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+});
